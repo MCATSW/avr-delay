43bf825acce1d741789e2146c5efd0f52a6994e5
meclondrej <ondrej.mecl@volny.cz>
2024-10-23 20:14:53 +0200

    Fixed missing cycle count calculation logic

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,27 @@ use core::arch::asm;
 
 /// Makes a delay lasting the specified duration (in microseconds).
 pub fn delay_us(duration_us: u16) {
+    /*
+     * the cycle formula:
+     * cycles = (delay_us / tick_time_us) / ticks_per_loop
+     * cycles = (delay_us / cpu_frequency^(-1)) / ticks_per_loop
+     * cycles = (delay_us / 16^(-1)) / 4
+     * cycles = (delay_us * 16) / 4
+     * cycles = delay_us * (16 / 4)
+     *
+     * convertor = 16 / 4
+     * cycles = delay_us * convertor
+     */
+    const CONVERTOR: f32 = 16.0 / 4.0;
+    // calculates the cycle count, adding a 0.5
+    // to guarantee ceiling on integer cast
+    let cycles: u16 = (duration_us as f32 * CONVERTOR + 0.5) as u16;
     unsafe {
         asm!(
-            "1: sbiw {counter}, 1",
-            "brne 1b",
-            counter = inout(reg_iw) duration_us => _,
+            // 2 TICKS TOTAL
+            "1: sbiw {counter}, 1", // 2 TICKS
+            "brne 1b", // 1/2 TICKS
+            counter = inout(reg_iw) cycles => _,
         );
     }
 }
