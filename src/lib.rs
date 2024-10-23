a284178f2c1c7f10058ee76b699fd14925a635f1
meclondrej <ondrej.mecl@volny.cz>
2024-10-23 20:00:24 +0200

    Added the delay_us and delay_ms functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,22 @@
 #![no_std]
+#![feature(asm_experimental_arch)]
+
+use core::arch::asm;
+
+/// Makes a delay lasting the specified duration (in microseconds).
+pub fn delay_us(duration_us: u16) {
+    unsafe {
+        asm!(
+            "1: sbiw {counter}, 1",
+            "brne 1b",
+            counter = inout(reg_iw) duration_us => _,
+        );
+    }
+}
+
+pub fn delay_ms(duration_ms: u16) {
+    for _ in 0..duration_ms {
+        delay_us(1000);
+    }
+}
 
